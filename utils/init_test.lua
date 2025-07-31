29baddeeb6637d19cd434bd7fd509ba0257800e2
thewizardplusplus <thewizardplusplus@yandex.ru>
2025-07-31 06:45:01 +0500

    Issue #2: Copy the function `utils.clamp()` from the library `luaplot`

--- utils/init_test.lua
+++ utils/init_test.lua
@@ -22,3 +22,34 @@ function TestUtils.test_almost_equal_false()
 
   luaunit.assert_false(result)
 end
+
+-- utils.clamp()
+function TestUtils.test_clamp_middle()
+  local result = utils.clamp(32, 23, 42)
+
+  luaunit.assert_equals(result, 32)
+end
+
+function TestUtils.test_clamp_minimum()
+  local result = utils.clamp(23, 23, 42)
+
+  luaunit.assert_equals(result, 23)
+end
+
+function TestUtils.test_clamp_before_minimum()
+  local result = utils.clamp(22, 23, 42)
+
+  luaunit.assert_equals(result, 23)
+end
+
+function TestUtils.test_clamp_maximum()
+  local result = utils.clamp(42, 23, 42)
+
+  luaunit.assert_equals(result, 42)
+end
+
+function TestUtils.test_clamp_after_maximum()
+  local result = utils.clamp(43, 23, 42)
+
+  luaunit.assert_equals(result, 42)
+end
