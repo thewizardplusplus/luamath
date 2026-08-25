b4cd58bad511c97a4c86b1895afff7a627d34d2d
thewizardplusplus <thewizardplusplus@yandex.ru>
2026-08-25 05:52:36 +0500

    Fix #11: Add the `utils.sign()` function

--- utils/init.lua
+++ utils/init.lua
@@ -1,9 +1,26 @@
+-- luacheck: no max comment line length
+
 ---
 -- @module utils
 
 local assertions = require("luatypechecks.assertions")
 
 local utils = {}
+
+---
+-- @tparam number value
+-- @treturn -1|0|1 -1 for a negative value, 0 for zero, or 1 for a positive value
+function utils.sign(value)
+  assertions.is_number(value)
+
+  if value < 0 then
+    return -1
+  elseif value > 0 then
+    return 1
+  end
+
+  return 0
+end
 
 ---
 -- @tparam number value
@@ -16,17 +33,18 @@ function utils.round(value, precision)
   assertions.is_number(value)
   assertions.is_integer(precision)
 
+  local value_sign = utils.sign(value)
   local factor = 10 ^ precision
   local scaled_value = value * factor
 
   -- compare the fractional part instead of adding 0.5 before truncation;
   -- adding it can round values just below a half up and change large integral
   -- floating-point values; see https://github.com/golang/go/issues/20100
-  local rounded_value = scaled_value >= 0
+  local rounded_value = value_sign >= 0
     and math.floor(scaled_value)
     or math.ceil(scaled_value)
   if math.abs(scaled_value - rounded_value) >= 0.5 then
-    rounded_value = rounded_value + (scaled_value >= 0 and 1 or -1)
+    rounded_value = rounded_value + value_sign
   end
 
   local result = rounded_value / factor
